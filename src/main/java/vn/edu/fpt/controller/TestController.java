0920d859d0e307bb0cd579d04a20e1c0da23d1ef
licod1997 <licod1997@gmail.com>
2018-10-07 15:47:56 +0900

    change upload file multipart to base64

--- src/main/java/vn/edu/fpt/controller/TestController.java
+++ src/main/java/vn/edu/fpt/controller/TestController.java
@@ -1,34 +1,32 @@
 package vn.edu.fpt.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import vn.edu.fpt.dto.DetectionResultDTO;
-import vn.edu.fpt.service.FileStorageService;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TestController {
-    @Autowired
-    private FileStorageService fileStorageService;
 
     @PostMapping( "/upload" )
-    public List<DetectionResultDTO> uploadFile( @RequestParam( "file" ) MultipartFile file ) {
-        String fileName = fileStorageService.storeFile( file );
+    public List<DetectionResultDTO> uploadFile( @RequestParam( "file" ) String fileBase64 ) throws Exception {
 
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path( "/downloadFile/" )
-                .path( fileName )
-                .toUriString();
+        // Normalize file name
+        byte[] imageByte = Base64.decodeBase64( fileBase64 );
+
+        String workingDir = System.getProperty("user.dir");
+
+        new FileOutputStream( workingDir + "/img.jpg" ).write( imageByte );
+
         List<DetectionResultDTO> list = new ArrayList<>();
-        list.add( new DetectionResultDTO("Dangerous Object", 0.6) );
-        list.add( new DetectionResultDTO("Dangerous Object", 0.7) );
-        list.add( new DetectionResultDTO("Dangerous Object", 0.8) );
+        list.add( new DetectionResultDTO( "Dangerous Object", 0.6 ) );
+        list.add( new DetectionResultDTO( "Dangerous Object", 0.7 ) );
+        list.add( new DetectionResultDTO( "Dangerous Object", 0.8 ) );
         return list;
     }
 }
