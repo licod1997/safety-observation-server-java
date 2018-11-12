ece5b9145d54d9b1af6825106458116efcd1be9c
anhntt08 <32409695+anhntt08@users.noreply.github.com>
2018-11-12 20:42:39 +0700

    edit entity notification

--- src/main/java/vn/edu/fpt/entity/Notification.java
+++ src/main/java/vn/edu/fpt/entity/Notification.java
@@ -10,68 +10,34 @@ public class Notification {
     @GeneratedValue( strategy = GenerationType.AUTO )
     @Column( name = "id" )
     private Long id;
-    @Column( name = "location" )
-    private String location;
+
+    @Column( name = "image_url" )
+    private String imageURL;
+
     @Temporal( TemporalType.TIMESTAMP )
     @Column( name = "time" )
     private Date time;
+
     @Column( name = "status" )
-    private String status;
-    @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY, targetEntity = NotificationStatus.class )
-    @JoinColumn( name = "notification_status_id", referencedColumnName = "id" )
-    private NotificationStatus notificationStatus;
+    private int status;
+
+    @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY, targetEntity = CameraLocation.class )
+    @JoinColumn( name = "camera_location", referencedColumnName = "id" )
+    private CameraLocation cameraLocation;
+
 
     public Notification() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId( Long id ) {
-        this.id = id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation( String location ) {
-        this.location = location;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime( Date time ) {
-        this.time = time;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus( String status ) {
-        this.status = status;
-    }
-
-    public NotificationStatus getNotificationStatus() {
-        return notificationStatus;
-    }
-
-    public void setNotificationStatus( NotificationStatus notificationStatus ) {
-        this.notificationStatus = notificationStatus;
-    }
 
     @Override
     public String toString() {
         return "Notification{" +
                 "id=" + id +
-                ", location='" + location + '\'' +
+                ", location='" + cameraLocation + '\'' +
                 ", time=" + time +
                 ", status='" + status + '\'' +
-                ", notificationStatus=" + notificationStatus +
+                ", notificationStatus=" + status +
                 '}';
     }
 }
