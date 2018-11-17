4fa5559a46023f20e2c789e7c5fee9d21e64e8f7
ANHNDNSE61922 <nhatanh2996@gmail.com>
2018-11-17 15:49:50 +0700

    [Nhat Anh] add thêm user vào traningFile, controller và html phải sửa lại khi có login

--- src/main/java/vn/edu/fpt/controller/TrainController.java
+++ src/main/java/vn/edu/fpt/controller/TrainController.java
@@ -43,6 +43,7 @@ public class TrainController {
 
     @PostMapping("/uploadMultipleTrainFiles")
     public List<UploadFileResponse> uploadMultipleTrainFiles( @RequestParam("files") MultipartFile[] files) {
+        //add them param User_ID khi đã có login
         List<UploadFileResponse> list = Arrays.asList(files)
                 .stream()
                 .map(file -> uploadFile(file))
@@ -54,6 +55,8 @@ public class TrainController {
             trainingFile.setFileDirectory(fileStorageService.getFileStorageLocation().toString() );
             trainingFile.setFileName( file.getOriginalFilename() );
             trainingFile.setTimeUpload( time );
+            //set thêm user vào khi có chức năng login
+            
             trainService.addFile( trainingFile );
 
         }
