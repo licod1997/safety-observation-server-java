0d0a34158f0cf0985826a43de6af714cd471fbf2
licod1997 <licod1997@gmail.com>
2018-11-25 00:55:58 +0900

    Add demo api for testing login on android

--- src/main/java/vn/edu/fpt/controller/DemoController.java
+++ src/main/java/vn/edu/fpt/controller/DemoController.java
@@ -2,11 +2,11 @@ package vn.edu.fpt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import vn.edu.fpt.dto.DemoDTO;
-import vn.edu.fpt.dto.NotificationDTO;
 import vn.edu.fpt.service.DemoService;
 
 @RestController
@@ -19,8 +19,12 @@ public class DemoController {
             consumes = "application/json",
             produces = "application/json" )
     public ResponseEntity login( @RequestBody DemoDTO demoDTO ) {
-        return ResponseEntity.ok( demoService.login(demoDTO.getUsername(), demoDTO.getPassword()) );
+        return ResponseEntity.ok( demoService.login( demoDTO.getUsername(), demoDTO.getPassword() ) );
     }
 
 
+    @GetMapping( "/android-demo-auth" )
+    public ResponseEntity response() {
+        return ResponseEntity.ok().body("Successfully-accessed-the-system-by-adding-Jwt-to-the-request-parameter");
+    }
 }
